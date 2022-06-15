d04aef3f9661e9a67d19ff47fe7457412ed06287
ehqkr <ghyoo0915@naver.com>
2022-06-15 10:57:44 +0900

    유광현 - 제품  등록 구현중

--- admin/custom_lib/template/order_info_process.php
+++ admin/custom_lib/template/order_info_process.php
@@ -3,6 +3,7 @@
 
     $mode = $_GET["mode"];
     if($mode === 'update'){
+        // 아래 3줄 (7~9) 메소드화 했으면 좋겠음.
         $jsonStr = $_GET["jsonStr"];
         $info = json_decode($jsonStr, true);
         $tel = $info["tel1"].'-'.$info["tel2"].'-'.$info["tel3"];
@@ -29,7 +30,7 @@
     }else if($mode === 'delete'){
         $sql = 'delete from item_order_info where id = '.$_GET["id"];
         $result = connect($sql);
-        
+
         echo '삭제되었습니다.';
     }
 
